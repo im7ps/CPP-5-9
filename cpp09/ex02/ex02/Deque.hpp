7bfd791a48386ae7f783ed6a37b8b3ea61dfa040
Stefano Gerace <sgerace@e3r11p3.42roma.it>
2023-07-06 17:28:55 +0200

    completo e pulito, da testare

--- cpp09/ex02/ex02/Deque.hpp
+++
@@ -1,5 +0,0 @@
-#include <deque>
-#include <iostream>
-
-std::deque<int> merge2(const std::deque<int>& left, const std::deque<int>& right);
-std::deque<int> mergeInsertionSort2(const std::deque<int>& inputDeque);
--- cpp09/ex02/ex02/Vector.hpp
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include <iostream>
-
-std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right);
-std::vector<int> mergeInsertionSort(const std::vector<int>& inputVector);
